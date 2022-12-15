9243fec2ff1c820d48d475b7b16c274f72d02b95
novipal <serkan.palaz@novi-education.nl>
2022-12-15 13:10:47 +0100

    Intial

---
+++ src/main/java/nl/novi/autogarage/AutoGarageApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.autogarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGarageApplication.class, args);
+    }
+
+}
